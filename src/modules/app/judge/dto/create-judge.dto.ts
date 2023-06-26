e7a486a90258787044cb1f8bb40d9fa720f0e6ad
sojebsikder <sojebsikder@gmail.com>
2023-06-26 12:47:27 +0600

    main

--- src/modules/app/judge/dto/create-judge.dto.ts
+++ src/modules/app/judge/dto/create-judge.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateJudgeDto {
+  @ApiProperty()
   code: string;
+  @ApiProperty()
   language: string;
 }
--- src/modules/app/judge/judge.controller.ts
+++ src/modules/app/judge/judge.controller.ts
@@ -6,15 +6,22 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { JudgeService } from './judge.service';
 import { CreateJudgeDto } from './dto/create-judge.dto';
 import { UpdateJudgeDto } from './dto/update-judge.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 
+@ApiBearerAuth()
+@ApiTags('judge')
+@UseGuards(JwtAuthGuard)
 @Controller('judge')
 export class JudgeController {
   constructor(private readonly judgeService: JudgeService) {}
 
+  @ApiOperation({ summary: 'Code submission' })
   @Post()
   async create(@Body() createJudgeDto: CreateJudgeDto) {
     return await this.judgeService.create(createJudgeDto);
