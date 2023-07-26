8cca977379d2afa17e500bde9b07c1ea1cbaa0d5
sojebsikder <sojebsikder@gmail.com>
2023-07-26 23:27:07 +0600

    added socket

--- src/modules/app/judge/processors/judge.processor.ts
+++ src/modules/app/judge/processors/judge.processor.ts
@@ -1,10 +1,14 @@
 import { Process, Processor } from '@nestjs/bull';
 import { Job } from 'bull';
 import { JudgeService } from '../judge.service';
+import { SocketGateway } from '../../../../providers/socket/socket.gateway';
 
 @Processor('judge-queue')
 export class JudgeProcessor {
-  constructor(private judgeService: JudgeService) {}
+  constructor(
+    private judgeService: JudgeService,
+    private readonly socketGateway: SocketGateway,
+  ) {}
   /**
    * process job
    * @param job
@@ -12,10 +16,20 @@ export class JudgeProcessor {
    */
   @Process('code-execution')
   async codeExecution(job: Job<any>) {
-    await this.judgeService._processJudge({
+    const response = await this.judgeService._processJudge({
       createJudgeDto: job.data.createJudgeDto,
       operation: job.data.operation,
       userId: job.data.userId,
+    });
+
+    const userId = job.data.userId;
+    const data = {
+      response: response,
+    };
+    // fire event to client
+    this.socketGateway.server.to(userId.toString()).emit('message', {
+      from: userId.toString(),
+      data: data,
     });
   }
 }
